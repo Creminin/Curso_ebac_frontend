eb3220fc2853e616408e5dd77dbce1c1cc2d2707
Nathan Lucas <114629361+Creminin@users.noreply.github.com>
2023-03-28 19:05:53 -0300

    adicionando arquivos da tarefa

---
+++ main.js
@@ -0,0 +1,5 @@
+"use strict";
+const Multiplicacao = (number1, number2) => number1 * number2;
+console.log(Multiplicacao(1, 2));
+const Saudacao = (nome) => `Olá ${nome}`;
+console.log(Saudacao('Nathan'));
---
+++ main.ts
@@ -0,0 +1,7 @@
+const Multiplicacao = (number1: number, number2: number): number => number1 * number2;
+
+console.log(Multiplicacao(1, 2));
+
+const Saudacao = (nome: string) => `Olá ${nome}`;
+
+console.log(Saudacao('Nathan'))
